4f2aeb123136ed455101656a8f3179d6cbfe252f
ManuelPiresLuis <manuelpiresluis@gmail.com>
2025-04-06 00:07:38 +0100

    fix: update database connection string to use hardcoded URL

--- BACKEND/src/models/db.js
+++ BACKEND/src/models/db.js
@@ -3,7 +3,7 @@ import mysql from 'mysql2';
 
 dotenv.config();
 
-const db= mysql.createConnection(process.env.DB_URL);
+const db= mysql.createConnection("mysql://root:SRcbBobVheyvSHWrCgSUgiQysLizbqIV@turntable.proxy.rlwy.net:52898/railway");
 
 db.connect((err) => {
   if (err) {
