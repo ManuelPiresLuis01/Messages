1369c8c04e735a87d64cfa362e0c5f893a8a0c4e
ManuelPiresLuis <manuelpiresluis@gmail.com>
2025-04-05 23:51:22 +0100

    refactor database connection; simplify connection setup using URL_DB environment variable

--- BACKEND/src/models/db.js
+++ BACKEND/src/models/db.js
@@ -4,13 +4,7 @@ import  dotenv from "dotenv"
 dotenv.config();
 
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: 3306,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+const db = mysql.createConnection(process.env.URL_DB);
 
 db.connect((err) => {
   if (err) {
