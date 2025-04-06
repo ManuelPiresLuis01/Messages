b1c0241f156b31c0a0c1381c6dafa2d0aa08035c
ManuelPiresLuis <manuelpiresluis@gmail.com>
2025-04-06 13:04:44 +0100

    refactor: standardize import alias for BrowserRouter in App component

--- FRONTEND/Messages/src/App.tsx
+++ FRONTEND/Messages/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Messages from "./assets/pages/messagesPage/Messages"
 import { SignIn, SignUp } from "./assets/pages/sign/Sign"
 import Main from "./assets/pages/mainPage/MAin"
@@ -8,11 +8,11 @@ import ProtectedLayout from "./services/protected"
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
-          
+
           <Route element={<ProtectedLayout />}>
             <Route path="/" element={<Main />} />
             <Route path="/messages/:id/:chatId" element={<Messages />} />
@@ -20,7 +20,7 @@ function App() {
 
           <Route path="*" element={<h1>444 PAGE NOT FOUND</h1>} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 }
