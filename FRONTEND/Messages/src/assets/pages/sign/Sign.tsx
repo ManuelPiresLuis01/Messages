71762305676c56889d54bdf55271c4e2fe298fda
ManuelPiresLuis <manuelpiresluis@gmail.com>
2025-04-06 12:59:30 +0100

    refactor: improve code formatting and consistency in Sign components

--- FRONTEND/Messages/src/assets/pages/sign/Sign.tsx
+++ FRONTEND/Messages/src/assets/pages/sign/Sign.tsx
@@ -1,15 +1,15 @@
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import style from "./Style.module.css"
 import Api from '../../../services/api';
 import { useState } from 'react';
-import { ClipLoader} from 'react-spinners';
-import{ SuccessModal,SuccessLogin} from '../succes/succes';
+import { ClipLoader } from 'react-spinners';
+import { SuccessModal, SuccessLogin } from '../succes/succes';
 
 export function SignIn() {
     const [email, Setemail] = useState<string>("")
     const [password, Setpassword] = useState<string>("")
     const [loading, setLoading] = useState(false);
-    const [succes,setSucces] = useState <boolean> (false)
+    const [succes, setSucces] = useState<boolean>(false)
     const submit = async (e: React.FormEvent) => {
         if (email === "" && password === "") return;
         e.preventDefault()
@@ -21,7 +21,7 @@ export function SignIn() {
             localStorage.setItem("token", response.data.token)
         } catch (error) {
             console.error(error)
-        }finally {
+        } finally {
             setLoading(false);
         }
     }
@@ -29,7 +29,7 @@ export function SignIn() {
         <div className={style.sign}>
             <div className={style.bgLogin}></div>
             <div>
-                {succes && <SuccessLogin value='Login feito com sucesso'/>}
+                {succes && <SuccessLogin value='Login feito com sucesso' />}
                 <div className={style.form}>
                     <h1>Entrar</h1>
                     <form onSubmit={submit}>
@@ -37,7 +37,7 @@ export function SignIn() {
                         <input value={password} onChange={(e) => Setpassword(e.target.value)} type="password" placeholder="*************" />
                         <button type='submit' disabled={loading}>Entrar {loading && <ClipLoader color="#36d7b7" size={30} />}</button>
                         <Link to="/signup">Criar conta</Link>
-                    </form>     
+                    </form>
                 </div>
             </div>
         </div>
@@ -48,20 +48,24 @@ export function SignUp() {
     const [name, setName] = useState<string>("")
     const [email, Setemail] = useState<string>("")
     const [password, Setpassword] = useState<string>("")
-    const [succes,setSucces] = useState <boolean> (false)
+    const [succes, setSucces] = useState<boolean>(false)
+    const [loading, setLoading] = useState(false);
     const submit = async (e: React.FormEvent) => {
-
         e.preventDefault()
+        setLoading(true);
         try {
-             await Api.post("/signup", { name, email, password })
+            await Api.post("/signup", { name, email, password })
             setSucces(true)
+            setLoading(false);
         } catch (error) {
             console.error(error)
+        } finally {
+            setLoading(false);
         }
     }
     return (
         <div className={style.sign}>
-              {succes && <SuccessModal value='Conta criada com sucesso' where='signin'/>}
+            {succes && <SuccessModal value='Conta criada com sucesso' where='signin' />}
             <div className={style.bgSignUp}></div>
             <div>
                 <div className={style.form}>
@@ -70,7 +74,7 @@ export function SignUp() {
                         <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Seu nome" required />
                         <input value={email} onChange={(e) => Setemail(e.target.value)} type="email" placeholder='Seu email' required />
                         <input value={password} onChange={(e) => Setpassword(e.target.value)} type="password" placeholder="Sua password" required />
-                        <button type='submit'>Cadastrar</button>
+                        <button type='submit' disabled={loading}>Cadastrar{loading && <ClipLoader color="#36d7b7" size={30} />}</button>
                         <Link to="/signin">Ja tenho uma conta</Link>
                     </form>
                 </div>
