cf8e1979fc716444eba2f425bdcb98f84a7f1311
ManuelPiresLuis <manuelpiresluis@gmail.com>
2025-04-06 14:27:35 +0100

    refactor: switch from createConnection to createPool for improved database connection management

--- BACKEND/src/models/db.js
+++ BACKEND/src/models/db.js
@@ -3,12 +3,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  waitForConnections: true,   // Espera por conexões disponíveis se o pool estiver ocupado
+  connectionLimit: 10,        // Número máximo de conexões simultâneas
+  queueLimit: 0               // Não há limite para o número de requisições na fila
 });
 
 db.connect((err) => {
