0a3ba5d054ba943568c60d2c1272740c7216f92c
ManuelPiresLuis <manuelpiresluis@gmail.com>
2025-04-06 12:27:21 +0100

    refactor: update Vite configuration and add @types/node dependency

--- FRONTEND/Messages/vite.config.ts
+++ FRONTEND/Messages/vite.config.ts
@@ -1,6 +1,12 @@
-export default {
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
   server: {
-    host: '0.0.0.0', 
-    port: 3000, 
+    host: "0.0.0.0",
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    strictPort: true,
+    allowedHosts: ["https://envie-mensagens.onrender.com"],
   },
-};
+});
